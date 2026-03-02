3cb1ec2e21ae0ab53fcf662afe3e00dfa9c027e0
boris <bgrebennikovv@gmail.com>
2026-03-02 20:35:47 +0400

    add basket component

---
+++ src/main/java/org/skypro/skyshop/model/basket/ProductBasket.java
@@ -0,0 +1,31 @@
+package org.skypro.skyshop.model.basket;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.SessionScope;
+
+import java.util.*;
+
+
+@SessionScope
+@Component
+public class ProductBasket {
+
+    private final Map<UUID, Integer> productBasketItems;
+
+    public ProductBasket() {
+        this.productBasketItems = new HashMap<>();
+    }
+
+    public void add(String productId) {
+        productBasketItems.merge(
+                UUID.fromString(productId),
+                1, Integer::sum
+        );
+    }
+
+    public Map<UUID, Integer> getAll() {
+        return Collections.unmodifiableMap(productBasketItems);
+    }
+
+
+}
